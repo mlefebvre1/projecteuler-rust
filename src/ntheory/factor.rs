372c539c224a160863b32ea55e8c2dee744dbf7c
mlefebvre <michael.lefebvre.31@gmail.com>
2022-01-08 20:59:06 -0500

    add some more unittests

--- src/ntheory/factor.rs
+++ src/ntheory/factor.rs
@@ -38,3 +38,23 @@ pub fn proper_divisors_sum(n: usize) -> usize {
     }
     return proper_div_sum;
 }
+
+#[test]
+fn test_factorize() {
+    assert_eq!(factorize(2), [1, 2]);
+    assert_eq!(factorize(10), [1, 2, 5, 10]);
+    assert_eq!(factorize(13), [1, 13]);
+    assert_eq!(factorize(28), [1, 2, 4, 7, 14, 28]);
+    assert_eq!(factorize(788), [1, 2, 4, 197, 394, 788]);
+    assert_eq!(factorize(512), [1, 2, 4, 8, 16, 32, 64, 128, 256, 512]);
+}
+
+#[test]
+fn test_proper_divisors_sum() {
+    assert_eq!(proper_divisors_sum(2), 1);
+    assert_eq!(proper_divisors_sum(10), 8);
+    assert_eq!(proper_divisors_sum(13), 1);
+    assert_eq!(proper_divisors_sum(28), 28);
+    assert_eq!(proper_divisors_sum(788), 598);
+    assert_eq!(proper_divisors_sum(512), 511);
+}
--- src/ntheory/series.rs
+++ src/ntheory/series.rs
@@ -30,3 +30,22 @@ where
         Some(self.a.clone())
     }
 }
+
+#[test]
+fn test_fibonacci_usize() {
+    let fib = Fibonacci::<usize>::new();
+    let actual: Vec<usize> = fib.take(20).collect();
+    assert_eq!(
+        actual,
+        [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765]
+    );
+}
+
+#[test]
+fn test_fibonacci_biguint() {
+    use num::BigUint;
+    let mut fib = Fibonacci::<BigUint>::new();
+    let actual = fib.nth(100).unwrap();
+    let expected = BigUint::parse_bytes(b"573147844013817084101", 10).unwrap();
+    assert_eq!(actual, expected);
+}
