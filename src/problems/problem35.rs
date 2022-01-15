751085ad8b0c7a96bc433eda6143df99bb93d469
mlefebvre <michael.lefebvre.31@gmail.com>
2022-01-15 08:14:54 -0500

    speed up problem35

--- src/problems/problem35.rs
+++ src/problems/problem35.rs
@@ -2,42 +2,36 @@ use crate::ntheory::primes::{is_prime, sieves};
 use crate::utils::timeit;
 
 struct DigitRotations {
-    n: String,
+    n: Vec<char>,
 }
 
 impl DigitRotations {
-    pub fn new(_n: &str) -> Self {
-        Self {
-            n: String::from(_n),
-        }
+    pub fn new(mut _n: &[char]) -> Self {
+        Self { n: _n.to_vec() }
     }
-    fn rotation(_n: &str) -> String {
-        let mut digits: Vec<char> = _n.chars().collect();
-        let nb_digits = digits.len();
-        for i in 0..(nb_digits - 1) {
-            digits.swap(i, i + 1);
+    fn rotation(_n: &mut Vec<char>) {
+        for i in 0..(_n.len() - 1) {
+            _n.swap(i, i + 1);
         }
-        digits.into_iter().collect::<String>()
     }
 }
 
 impl Iterator for DigitRotations {
-    type Item = String;
-    fn next(&mut self) -> Option<String> {
+    type Item = Vec<char>;
+    fn next(&mut self) -> Option<Vec<char>> {
         let _n = self.n.clone();
-        self.n = Self::rotation(&self.n);
+        Self::rotation(&mut self.n);
         Some(_n)
     }
 }
 
 #[test]
 fn rotation_test() {
-    let n = "907";
+    let n = vec!['9', '0', '7'];
+    let rotations: Vec<Vec<char>> = DigitRotations::new(&n).take(n.len()).collect();
     assert_eq!(
-        DigitRotations::new(n)
-            .take(n.chars().count())
-            .collect::<Vec<String>>(),
-        ["907", "079", "790"]
+        rotations,
+        [['9', '0', '7'], ['0', '7', '9'], ['7', '9', '0']]
     )
 }
 
@@ -53,19 +47,19 @@ fn p() -> String {
 
     How many circular primes are there below one million?
     */
-    fn get_all_rotations(n: &str) -> std::iter::Take<DigitRotations> {
-        let nb_rotations = n.chars().count();
-        DigitRotations::new(n).take(nb_rotations)
+    fn get_all_rotations(n: usize) -> std::iter::Take<DigitRotations> {
+        let _n: Vec<char> = n.to_string().chars().collect();
+        DigitRotations::new(&_n).take(_n.len())
     }
-    fn is_circular(n: &str) -> bool {
-        let rotations = get_all_rotations(n);
+    fn is_circular(n: usize) -> bool {
+        let rotations = get_all_rotations(n).map(|r| r.into_iter().collect::<String>());
         let rotations_not_prime = rotations.filter(|x| !is_prime(x.parse::<usize>().unwrap()));
         rotations_not_prime.count() == 0
     }
 
     const MAX_PRIME: usize = 1e6 as usize;
     let primes = sieves(MAX_PRIME);
-    let circulars = primes.iter().filter(|n| is_circular(&(n.to_string())));
+    let circulars = primes.iter().filter(|&n| is_circular(*n));
     let nb_circulars = circulars.count();
     nb_circulars.to_string()
 }
