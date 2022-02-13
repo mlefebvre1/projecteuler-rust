a6e83978b61344090524add6cd0c34877d0b503e
Michaël Lefebvre <michael.lefebvre.31@gmail.com>
2022-02-12 22:08:46 -0400

    solve problem57

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod problems;
 mod series;
 mod utils;
 
-const PROBLEMS: [fn() -> String; 56] = [
+const PROBLEMS: [fn() -> String; 57] = [
     problems::problem01::solve,
     problems::problem02::solve,
     problems::problem03::solve,
@@ -60,6 +60,7 @@ const PROBLEMS: [fn() -> String; 56] = [
     problems::problem54::solve,
     problems::problem55::solve,
     problems::problem56::solve,
+    problems::problem57::solve,
 ];
 
 #[cfg(not(tarpaulin_include))]
@@ -71,7 +72,7 @@ fn main() {
 
 #[test]
 fn test_regression() {
-    const SOLUTIONS: [&str; 56] = [
+    const SOLUTIONS: [&str; 57] = [
         "233168",
         "4613732",
         "6857",
@@ -128,6 +129,7 @@ fn test_regression() {
         "376",
         "249",
         "972",
+        "153",
     ];
     for (problem, solution) in PROBLEMS.iter().zip(SOLUTIONS.iter()) {
         let result = problem();
---
+++ src/problems/problem57.rs
@@ -0,0 +1,37 @@
+use crate::utils::timeit;
+use num::BigUint;
+
+fn p() -> String {
+    /*
+    Square root convergents
+    Problem 57
+    It is possible to show that the square root of two can be expressed as an infinite continued fraction.
+    In the first one-thousand expansions, how many fractions contain a numerator with more digits than the denominator?
+    sqrt2 can be represented by the infinite fraction :
+        sqrt(2) = h(n)/k(n)
+        h(n) = a(n) * h(n-1) + h(n-2)
+        k(n) = a(n) * k(n-1) + k(n-2)
+        a(n) = 2
+
+    Turns out that the first fraction is for n=7 and then a pattern alternate between 5 and 8..
+    Meaning the problem can be solve by hand.. ceil((1000-7) / (5 + 8) * 2)
+    That would be boring so I kept the code :)
+    */
+    const MAX_N: usize = 1000;
+    let a = BigUint::from(2usize);
+    let mut h = vec![BigUint::from(3usize), BigUint::from(7usize)];
+    let mut k = vec![BigUint::from(2usize), BigUint::from(5usize)];
+    let mut total: usize = 0;
+    for n in 2..MAX_N {
+        let hn = &a * &h[n - 1] + &h[n - 2];
+        let kn = &a * &k[n - 1] + &k[n - 2];
+        if hn.to_radix_be(10).len() > kn.to_radix_be(10).len() {
+            total += 1;
+        }
+        h.push(hn);
+        k.push(kn);
+    }
+    total.to_string()
+}
+
+timeit::timeit!(Problem57, solve, p);
