974b86ec8349d0516e361d7eaea87df3211dc283
Michaël Lefebvre <michael.lefebvre.31@gmail.com>
2022-03-14 14:22:03 -0300

    solve problem64

---
+++ src/problems/problem64.rs
@@ -0,0 +1,43 @@
+use crate::series::square::Square;
+use crate::utils::timeit;
+use num::Integer;
+
+fn sqrt_continued_fraction_expansion_len(n: usize) -> usize {
+    let (m0, d0, a0) = (0, 1, (n as f64).sqrt().floor() as usize);
+    let (mut mn, mut dn, mut an) = (m0, d0, a0);
+    let criteria = 2 * an;
+    let mut len = 0;
+    while an != criteria {
+        mn = dn * an - mn;
+        dn = (n - mn.pow(2)) / dn;
+        an = (a0 + mn) / dn;
+        len += 1;
+    }
+    len
+}
+
+fn p() -> String {
+    /*
+    Odd period square roots
+    Problem 64
+
+    All square roots are periodic when written as continued fractions and can be written in the form:
+    For example, let us consider
+    If we continue we would get the following expansion:
+    The process can be summarised as follows:
+    It can be seen that the sequence is repeating. For conciseness, we use the notation
+    sqrt(23) = [4;(1,3,1,8)], to indicate that the block (1,3,1,8) repeats indefinitely.
+    The first ten continued fraction representations of (irrational) square roots are:
+    See : https://projecteuler.net/problem=64
+    Exactly four continued fractions, have an odd period.
+    How many continued fractions for have an odd period?
+    */
+    const MAX_N: usize = 10000;
+    let squares: Vec<usize> = Square::new().skip(2).take(MAX_N - 2).collect();
+    let candidates = (2..=MAX_N).filter(|n| !squares.contains(n));
+    let odd_period_sqrts =
+        candidates.filter(|&n| sqrt_continued_fraction_expansion_len(n).is_odd());
+    odd_period_sqrts.count().to_string()
+}
+
+timeit::timeit!(Problem64, solve, p);
