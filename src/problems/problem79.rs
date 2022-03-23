d8307824b484fd25059ec9fd1f1448064af35945
Michaël Lefebvre <michael.lefebvre.31@gmail.com>
2022-03-22 21:27:54 -0300

    solve problem79

---
+++ src/problems/problem79.rs
@@ -0,0 +1,93 @@
+use crate::utils::timeit;
+use std::fs;
+
+fn p() -> String {
+    /*
+    Passcode derivation
+    Problem 79
+
+    A common security method used for online banking is to ask the user for three random characters from a passcode. For
+    example, if the passcode was 531278, they may ask for the 2nd, 3rd, and 5th characters; the expected reply would be:
+    317.
+
+    The text file, keylog.txt, contains fifty successful login attempts.
+
+    Given that the three characters are always asked for in order, analyse the file so as to determine the shortest
+    possible secret passcode of unknown length.
+    */
+    let logins =
+        fs::read_to_string("src/problems/data/problem79.txt").expect("Problem opening the file");
+    let logins = logins.lines().collect::<Vec<&str>>();
+    let mut digits_to_place = generate_digit_candidates(&logins);
+    let mut passcode = String::from("");
+    while !digits_to_place.is_empty() {
+        let digit = find_largest_digit(&logins, &digits_to_place);
+        let index = digits_to_place.iter().position(|&x| x == digit).unwrap();
+        digits_to_place.remove(index);
+        passcode += &digit.to_string();
+    }
+    passcode
+}
+
+fn generate_digit_candidates(logins: &[&str]) -> Vec<usize> {
+    let mut digit_presence = (0..10).map(|_| false).collect::<Vec<bool>>();
+    for login in logins.iter() {
+        for digit in login
+            .chars()
+            .map(|c| c.to_digit(10).unwrap() as usize)
+            .collect::<Vec<usize>>()
+        {
+            digit_presence[digit] = true;
+        }
+    }
+    digit_presence
+        .into_iter()
+        .enumerate()
+        .filter_map(|(digit, presence)| {
+            if presence {
+                return Some(digit);
+            }
+            None
+        })
+        .collect::<Vec<usize>>()
+}
+
+fn find_largest_digit(logins: &[&str], digits_to_place: &[usize]) -> usize {
+    let mut largest_digit = (0..10)
+        .map(|digit| digits_to_place.contains(&digit))
+        .collect::<Vec<bool>>();
+    for login in logins.iter() {
+        let login_len = login.chars().count();
+        // taking len - 1 because the left digit cannot be the last digit, we need a digit to compare with..
+        for (i, digit_to_the_left) in login
+            .chars()
+            .map(|c| c.to_digit(10).unwrap() as usize)
+            .enumerate()
+            .take(login_len - 1)
+        {
+            // try every digit until we get one that is a digit to place
+            if digits_to_place.contains(&digit_to_the_left) {
+                for digit_to_the_right in login
+                    .chars()
+                    .map(|c| c.to_digit(10).unwrap() as usize)
+                    .skip(i + 1)
+                {
+                    // if a digit is to the right of another digit, that means it can't be the largest digit..
+                    largest_digit[digit_to_the_right] = false;
+                }
+            }
+        }
+    }
+    largest_digit
+        .into_iter()
+        .enumerate()
+        .find_map(|(digit, still_true)| {
+            if still_true {
+                return Some(digit);
+            }
+            None
+        })
+        .unwrap()
+}
+
+timeit::timeit!(Problem79, solve, p);
